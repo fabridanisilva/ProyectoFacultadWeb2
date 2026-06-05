6afa53059e25c522ebce101d6dea1159363cef32
fabricio silva <fabridanisilva@gmail.com>
2026-06-04 21:55:09 -0300

    ahora ya tenemos la vista de registrar funcionando, tambien encripta la contraseña

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const pool = require('./db/database'); 
+const bcrypt = require('bcrypt');
 require('dotenv').config();
 
 const app = express();
@@ -35,4 +36,45 @@ app.get('/', async (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+});
+
+
+// con esto se va a mostrar el formulario
+app.get('/registro', (req, res) => {
+    res.render('register', { title: 'Registro - Proyect' });
+});
+
+// esto es para procesar el registro 
+app.post('/registro', async (req, res) => {
+    // obtenemos o guardamos los datos que vienen del formulario
+    const { username, email, password, password2 } = req.body;
+
+    if (password !== password2) {
+        return res.status(400).send('<h1>Error: Las contraseñas no coinciden.</h1><a href="/registro">Volver al registro</a>');
+    }
+    try {
+        // este es elnivel de seguridad del encriptado
+        const saltRounds = 10;
+        // hasheamos la contraseña
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        // guardamos el usuario en la base de datos con la contraseña encriptada
+        await pool.query(
+            'INSERT INTO users (username, email, password) VALUES ($1, $2, $3)',
+            [username, email, hashedPassword]
+        );
+
+        // si todo sale bien, lo mandamos al inicio
+        console.log(`✅ Nuevo usuario registrado: ${username}`);
+        res.redirect('/');
+        
+    } catch (err) {
+        console.error(err);
+        //capturamos excepciones 
+        if (err.code === '23505') {
+            res.status(400).send('<h1>Error: El nombre de usuario o el correo ya están en uso.</h1><a href="/registro">Volver</a>');
+        } else {
+            res.status(500).send('Error interno del servidor al registrar el usuario.');
+        }
+    }
 });
