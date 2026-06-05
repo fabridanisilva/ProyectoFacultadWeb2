80089f61084f6c46283eac6fc5aeb5958cea9aeb
fabricio silva <fabridanisilva@gmail.com>
2026-06-04 23:10:48 -0300

    ya pude agregar la vista del login para iniciar secion

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const pool = require('./db/database'); 
 const bcrypt = require('bcrypt');
+const session = require('express-session');
 require('dotenv').config();
 
 const app = express();
@@ -15,6 +16,20 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// configuración de la sesión
+app.use(session({
+    secret: 'mi_secreto_super_seguro', 
+    resave: false,
+    saveUninitialized: false
+}));
+
+// esto es para pasar los datos del usuario a todas las vistas Pug y que no se olvide que iniciamos secion en cada vista o pagina
+app.use((req, res, next) => {
+    res.locals.usuarioActual = req.session.user;
+    next();
+});
+
 
 // esto es una prueba para ver si funciona la conexion a bd
 app.get('/', async (req, res) => {
@@ -65,7 +80,7 @@ app.post('/registro', async (req, res) => {
         );
 
         // si todo sale bien, lo mandamos al inicio
-        console.log(`✅ Nuevo usuario registrado: ${username}`);
+        console.log(`Nuevo usuario registrado: ${username}`);
         res.redirect('/');
         
     } catch (err) {
@@ -77,4 +92,52 @@ app.post('/registro', async (req, res) => {
             res.status(500).send('Error interno del servidor al registrar el usuario.');
         }
     }
+});
+
+
+// con esto mostramos el login o inicio de secion
+app.get('/login', (req, res) => {
+    res.render('login', { title: 'Iniciar Sesión - Proyect' });
+});
+
+// procesamos el login
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        // buscamos al usuario en la base de datos
+        const result = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+        
+        if (result.rows.length === 0) {
+            return res.status(401).send('<h1>Error: Usuario no encontrado.</h1><a href="/login">Volver</a>');
+        }
+
+        const user = result.rows[0];
+
+        // comparamos la contraseña ingresadas
+        const match = await bcrypt.compare(password, user.password);
+        
+        if (!match) {
+            return res.status(401).send('<h1>Error: Contraseña incorrecta.</h1><a href="/login">Volver</a>');
+        }
+
+        // si todo está bien, guardamos al usuario en la sesión
+        req.session.user = {
+            username: user.username
+        };
+
+        console.log(`Sesión iniciada: ${user.username}`);
+        res.redirect('/');
+        
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error interno del servidor al iniciar sesión.');
+    }
+});
+
+// cerramos la seciuon
+app.get('/logout', (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/');
+    });
 });
